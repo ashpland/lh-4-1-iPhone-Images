3b75e9434d9cd39a412997eb8cf92d1ca183e2d5
Andrew Shopland <hi@ashp.land>
2017-10-23 16:30:24 -0700

    worked through sample code

--- iPhone Images/ViewController.m
+++ iPhone Images/ViewController.m
@@ -9,6 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *theImageView;
 
 @end
 
@@ -17,12 +18,35 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    NSURL *urlToDownload = [NSURL URLWithString:@"http://imgur.com/CoQ8aNl.png"]; // 1
+    
+    NSURLSessionConfiguration *configuration = [NSURLSessionConfiguration defaultSessionConfiguration]; // 2
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:configuration]; // 3
+    
+    NSURLSessionDownloadTask *downloadTask = [session downloadTaskWithURL:urlToDownload
+                                                        completionHandler:^(NSURL * _Nullable locationOfDownloadedFile,
+                                                                            NSURLResponse * _Nullable response,
+                                                                            NSError * _Nullable error) {
+                                                            if (error) { // 1
+                                                                // Handle the error
+                                                                NSLog(@"error: %@", error.localizedDescription);
+                                                                return;
+                                                            }
+                                                            
+                                                            NSData *downloadedRawData = [NSData dataWithContentsOfURL:locationOfDownloadedFile];
+                                                            UIImage *imageFromDownloadedRawData = [UIImage imageWithData:downloadedRawData]; // 2
+                                                            
+                                                            [[NSOperationQueue mainQueue] addOperationWithBlock:^{
+                                                                // This will run on the main queue
+                                                                
+                                                                self.theImageView.image = imageFromDownloadedRawData; // 4
+                                                            }];
+        
+    }]; // 4
+    
+    [downloadTask resume]; // 5
+    
 }
 
 
